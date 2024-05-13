310831e5262924199c266d67f72dea3415624f44
MiracleTenco <MiracleTenco@gmail.com>
2024-05-14 07:59:49 +0800

    注藥訊息
    提交測試訊息04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// test01-04
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+//test01-04
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
